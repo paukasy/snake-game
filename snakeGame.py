30166354862c1a83b6cb1fa323356920e8954db9
paukasy <paukaasy@gmail.com>
2024-12-19 19:31:55 -0500

    made snake move

--- snakeGame.py
+++ snakeGame.py
@@ -50,12 +50,12 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 snake.direction = Vector2(0,-1)
-            if event.key == pygame.K_DOWN:
-                snake.direction == Vector2(0,1)
             if event.key == pygame.K_RIGHT:
-                snake.direction == Vector2(1,0)
+                snake.direction = Vector2(1,0)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
             if event.key == pygame.K_LEFT:
-                snake.direction == Vector2(-1,0)
+                snake.direction = Vector2(-1,0)
 
     screen.fill((175,215,70))
     fruit.draw_fruit()
